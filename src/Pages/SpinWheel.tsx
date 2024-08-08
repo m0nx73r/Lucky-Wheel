a91d2945dfb48f67760f4ad8dad2531efcf78ed1
maheep05 <maheepsingh458@gmail.com>
2024-08-09 00:56:50 +0530

    fixes

--- src/Pages/SpinWheel.tsx
+++ src/Pages/SpinWheel.tsx
@@ -22,10 +22,22 @@ const SpinWheel = memo(() => {
     }
   };
 
+  const renderImages = () => {
+    // Create an array of image elements based on the number of spins
+    return Array.from({ length: spins }, (_, index) => (
+      <img
+        key={index} // Provide a unique key for each element
+        src={spin}
+        alt={`Spin ${index + 1}`}
+        className="h-12 md:h-14"
+      />
+    ));
+  };
+
   return (
     <div className="relative overflow-hidden bg-[#4c0e30] min-h-screen flex flex-col items-center px-4 md:px-8">
       <div
-        className="absolute p-2 top-4 right-4 md:top-6 md:right-6 cursor-pointer"
+        className="absolute p-2 top-4 z-50 right-4 md:top-6 md:right-6 cursor-pointer"
         onClick={toggleSpeaker}
       >
         {speakerOff ? <SpeakerOff /> : <SpeakerOn />}
@@ -42,15 +54,15 @@ const SpinWheel = memo(() => {
         Lucky Wheel
       </span>
 
-        <LuckyWheel
-          onSpin={handleSpin} // onspin function
-          remainingSpins={spins} // handling remaining spins as spin will be disabled after 3
-          speakerOff={speakerOff} // Pass the speakerOff state
-        />
+      <LuckyWheel
+        onSpin={handleSpin} // onspin function
+        remainingSpins={spins} // handling remaining spins as spin will be disabled after 3
+        speakerOff={speakerOff} // Pass the speakerOff state
+      />
 
       <div className="text-base md:text-xl bg-[#002a74] p-3 md:p-4 rounded-lg w-full max-w-md flex flex-col items-center gap-4 font-semibold text-white mt-6 mb-4">
-        {spins} Lucky Spin Available
-        <img src={spin} alt="" className="h-12 md:h-14" />
+        {spins > 0 ? `${spins} Lucky Spins Available` : "No Spins Available"}
+        <span className="flex flex-row items-center">{renderImages()}</span>
       </div>
     </div>
   );
