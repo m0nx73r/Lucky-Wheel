e8d8045eae9a79451e15186b6964bcf3cf570f9e
Aditya <naammkyahai@gmail.com>
2024-09-04 18:10:26 +0530

    changes in address

--- src/Pages/Address.tsx
+++ src/Pages/Address.tsx
@@ -103,7 +103,7 @@ export default function Address() {
                             name="addressType"
                             id="Home"
                             value="Home"
-                            className="form-radio text-blue-600"
+                            className="form-radio checked:bg-[#00bcfc]"
                         />
                         <span>Home</span>
                     </label>
@@ -113,7 +113,7 @@ export default function Address() {
                             name="addressType"
                             id="Office"
                             value="Office"
-                            className="form-radio text-blue-600"
+                            className="form-radio checked:bg-[#00bcfc]"
                         />
                         <span>Office</span>
                     </label>
@@ -135,7 +135,7 @@ export default function Address() {
                     <p className="font-semibold text-sm">Set as Default address</p>
                     <label className="inline-flex items-center cursor-pointer">
                         <input type="checkbox" value="" className="sr-only peer" />
-                        <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-text-[#00bcfc] rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bgtext-[#00bcfc]"></div>
                     </label>
                 </div>
 
